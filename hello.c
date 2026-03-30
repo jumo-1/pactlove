89ec34c47ec60d0b37c37d5c4c35320921b28d5b
limichai <3319585182@qq.com>
2026-03-30 14:41:36 +0800

    加入了练习GCC编译的源文件

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"hello.h"
+#define LLJ 774
+const int n=9;
+int main(){
+	int m=LLJ;
+	int x;
+	x=add(m,n);
+	
+	printf("hello!m=%d,n=%d\n",m,n);
+
+		
+
+	return 0;
+}
---
+++ hello1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include"hello.h"
+int add(int n,int m){
+	printf("n+m=%d\n",n+m);
+	return 0;
+}
