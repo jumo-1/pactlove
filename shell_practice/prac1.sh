5ce6ed2896c8c960daa1e241a4bee5b6b1d45c2f
limichai <3319585182@qq.com>
2026-03-31 17:50:49 +0800

    整理练习区域

---
+++ shell_practice/prac1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+SEQ=$(seq 1 10)
+#for i in $SEQ;do
+#	touch  test_$i.txt
+#done
+
+#for i in $SEQ;do
+#	if [ $((i%2)) -eq 0 ]; then
+#		mv test_$i.txt test_$i.txt.bak
+#	fi
+#done
+
+#for file in test_*.txt.bak;do
+#	num=$(echo "$file" |awk -F'[_.]' '{print $2}')
+#	if [ $((num%2)) -eq 0 ] ; then
+#		mv $file $file.bak
+#	fi
+#done
+
+#for file in test_*.txt.bak;do
+#	temp=${file#*_}
+#	num=${temp%%.*}
+#	if [ $num -eq 3 ] ; then
+#		mv $file test_$num.txt
+#	fi
+#done
+
+for file in test_*.txt.bak.bak;do
+        temp=${file#*_}
+        num=${temp%%.*}
+        mv $file test_$num.txt 
+done
---
+++ shell_practice/prac2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+name="Avail"
+DATE=$(date)
+NUM=$(ls -l | grep "^-rwx" | wc -l)
+for i in $(seq 1 7);do
+	na=$(df -h / |awk -v col=$i 'NR==1 {print $col}')
+       if [ $na == $name ]; then
+	       av=$(df -h / | awk -v col=$i 'NR==2 {print $col}')
+       fi
+done
+file="report.txt"
+touch $file
+echo "====================" > $file
+echo "time:$DATE" >> $file
+echo "====================" >> $file
+echo "sum:$NUM" >> $file
+echo "====================" >> $file
+echo "avail:$av" >> $file
+echo "====================" >> $file
+
