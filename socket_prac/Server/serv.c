345e7ae8af5339000f4fe4415c5465360bd92082
limichai <3319585182@qq.com>
2026-04-12 17:24:43 +0800

    添加信号处理，增强鲁棒性

--- socket_prac/Server/serv.c
+++ socket_prac/Server/serv.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<signal.h>
 #include<netinet/in.h>
 #include<sys/socket.h>
 #include<sys/types.h>
@@ -9,7 +10,16 @@
 #include<string.h>
 #include<arpa/inet.h>
 #include <sys/epoll.h>
+#include <errno.h>  
+volatile sig_atomic_t stop = 0;
+void handle_sigint(int sig) {
+    stop = 1; // 设置标志位
+}
 int main(){
+	//【重要修复】忽略SIGPIPE信号，防止客户端断开时服务器崩溃
+	signal(SIGPIPE, SIG_IGN); 
+	// 处理SIGINT信号，优雅关闭服务器
+	signal(SIGINT, handle_sigint);
 	int lfd=socket(AF_INET,SOCK_STREAM,0);
 	if(lfd==-1){
 		perror("socket();");
@@ -20,6 +30,10 @@ int main(){
 	serv_addr.sin_port = htons(8888);
 	serv_addr.sin_family=AF_INET;
 	socklen_t len=sizeof(struct sockaddr_in);
+	//【重要修复】设置SO_REUSEADDR选项，允许地址重用，避免绑定失败
+	int opt=1;
+	setsockopt(lfd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+	//
 	if(bind(lfd,(struct sockaddr *)&serv_addr,len)==-1){
 		perror("bind();");
 		return 0;
@@ -33,9 +47,17 @@ int main(){
 			perror("listen();");
 			return 0;
 		}
-	while(1){
+	while(!stop) { // 循环直到收到SIGINT信号
 		
 		int num=epoll_wait(epfd,events,10,-1);
+		if(num==-1){
+			if (errno == EINTR) {
+				// 【重要修复】处理被信号中断的情况，继续等待事件
+				continue;
+			}
+			perror("epoll_wait();");
+			return 0;
+		}
 		for(int i=0;i<num;i++){
 			if(events[i].data.fd==lfd){
 				struct sockaddr_in clie_addr;
@@ -63,6 +85,9 @@ int main(){
 	
 	
 	}
+	
+	
+	printf("Shutting down server...\n");
 	close(lfd);
 	return 0;
 
