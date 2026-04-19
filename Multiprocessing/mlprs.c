2330a785378f0436a0cd5a4641348cf182c13f7a
limichai <3319585182@qq.com>
2026-04-19 19:24:11 +0800

    共享内存和信号量的练习

--- Multiprocessing/mlprs.c
+++ Multiprocessing/mlprs.c
@@ -3,9 +3,42 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/msg.h>
 #include <string.h>
-#define NUM_CHILDREN 5
+#define NUM_CHILDREN 10
+struct My_shm{
+    int data;
+    char str[100];
+    int competition;
+};
+// 初始化信号量
+int init_sem(int semid,int value){ 
+    union semun {
+        int val;               // 用于 SETVAL
+        struct semid_ds *buf;  // 用于 IPC_STAT 和 IPC_SET
+        unsigned short *array; // 用于 GETALL 和 SETALL
+    } sem_union;
+    sem_union.val = value;
+    return semctl(semid, 0, SETVAL, sem_union);
+ 
+}
+// P操作
+int P(int semid){
+    struct sembuf sem_b={0,-1,SEM_UNDO}; // 对信号量进行P操作，SEM_UNDO表示如果进程异常退出，系统会自动恢复信号量的值
+    
+    return semop(semid, &sem_b, 1);
+}
+// V操作
+int V(int semid){
+    struct sembuf sem_b={0,1,SEM_UNDO}; // 对信号量进行V操作，SEM_UNDO表示如果进程异常退出，系统会自动恢复信号量的值
+    return semop(semid, &sem_b, 1);
+}
+// 删除信号量
+int del_sem(int semid){
+    return semctl(semid, 0, IPC_RMID);  
+}
 void handle_sigint(int sig){
     //printf("父进程%d收到信号%d，继续执行...\n",getpid(),sig);
     // -1 表示回收任意子进程，WNOHANG 表示非阻塞回收子进程
@@ -29,6 +62,31 @@ int main(){
         long msg_type; // 消息类型
         char msg_text[100]; // 消息内容
     } message;
+    //共享内存的初始化
+    key_t k1=ftok("sharedmemfile", 65);
+    int shmid=shmget(k1, 1024, 0666 | IPC_CREAT);
+    if(shmid==-1){
+        perror("shmget error");
+        exit(1);
+    }
+    void *shmaddr=shmat(shmid,NULL,0); // 将共享内存附加到进程地址空间
+    if(shmaddr==(void*)-1){
+        perror("shmat error");
+        exit(1);
+    }
+    struct My_shm *const my_shm=(struct My_shm*)shmaddr; // 将共享内存地址转换为结构体指针
+    my_shm->competition=0; // 初始化竞争变量
+    // 信号量的初始化
+    key_t k2=ftok("semfile", 65);
+    int semid=semget(k2, 1, 0666 | IPC_CREAT);
+    if(semid==-1){
+        perror("semget error");
+        exit(1);
+    }
+    if(init_sem(semid, 1)==-1){
+        perror("init_sem error");
+        exit(1);
+    };
     for(int i=0;i<NUM_CHILDREN;i++){
         pid[i]=fork();
         if(pid[i]<0){
@@ -77,13 +135,55 @@ int main(){
                 
                 //execlp("ps","ps","-ef",NULL); // 子进程执行 ps -ef 命令
             }
+            if(i==5){
+                //int *int_ptr=(int*)shmaddr; // 将共享内存地址转换为整数指针
+                //*int_ptr=42; // 向共享内存写入数据
+                //printf("子进程%d:%d向共享内存写入数据：%d\n",getpid(),i,*int_ptr);
+                my_shm->data=41;
+                printf("子进程%d:%d向共享内存写入数据：%d\n",getpid(),i,my_shm->data);
+            }
+            if(i==6){
+                strcpy(my_shm->str,"Hello from  6!"); // 向共享内存写入字符串
+                printf("子进程%d:%d向共享内存写入字符串：%s\n",getpid(),i,my_shm->str);
+            }
+            if(i==7){
+                P(semid); // P操作，进入临界区
+                for(int j=0;j<1000000;j++){
+                    
+                    my_shm->competition++; // 竞争变量自增
+                    
+                }
+                V(semid); // V操作，离开临界区
+            }
+            if(i==8){
+                
+                P(semid); // P操作，进入临界区
+                for(int j=0;j<1000000;j++){
+                    
+                    my_shm->competition++; // 竞争变量自增
+                    
+                }
+                V(semid); // V操作，离开临界区
+                
+            }
+            if(i==9){
+                
+                P(semid); // P操作，进入临界区
+                for(int j=0;j<1000000;j++){
+                    
+                    my_shm->competition++; // 竞争变量自增
+                    
+                }
+                V(semid); // V操作，离开临界区
+                
+            }
             exit(0); // 子进程退出
         }else{
             continue; // 父进程继续创建下一个子进程
         }
     
     }
-
+    
     for(int i=0;i<2;i++){
         msgrcv(msgid,&message, sizeof(message.msg_text),0,0); // 父进程从消息队列接收消息
         printf("父进程%d从消息队列接收到消息：%s",getpid(),message.msg_text);
@@ -116,6 +216,15 @@ int main(){
     } else {
         printf("消息队列已成功从内核中销毁。\n");
     }
-    
+    printf("父进程%d从共享内存读取到数据：%d\n",getpid(),my_shm->data);
+    printf("父进程%d从共享内存读取到字符串：%s\n",getpid(),my_shm->str);
+    printf("父进程%d从共享内存读取到竞争变量的值：%d\n",getpid(),my_shm->competition);
+    int ok=shmdt(shmaddr); // 分离共享内存
+    if(ok==-1){
+        perror("shmdt error");
+        exit(1);
+    }
+    shmctl(shmid, IPC_RMID, NULL); // 删除共享内存
+    del_sem(semid); // 删除信号量
     return 0;
 }
